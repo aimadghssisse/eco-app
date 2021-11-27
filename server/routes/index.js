e5a16e24ddc676783cb0b5d20a89a9968b31d52d
aimadgh <ghssisseaimad@gmail.com>
2021-11-27 15:56:33 +0100

    first commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
-const { Router } = require("express");
-const { getProducts } = require("../controller/product");
-const middleware = require("../middleware");
-
-const router = Router();
-
-router.get('/getProducts', getProducts);
+// const { Router } = require("express");
+// const { getProducts } = require("../controller/product");
+// const middleware = require("../middleware");
+//
+// const router = Router();
+//
+// router.get('/getProducts', getProducts);
